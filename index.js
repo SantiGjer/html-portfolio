cfc18f1284b63c4952f9a1bc4cf2974bba0e0b3e
SantiGjer <134178465+SantiGjer@users.noreply.github.com>
2023-05-25 22:04:37 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+/*let flexbox =
+    document.querySelector(".flexbox")
+
+let index = 0;
+
+setInterval(function() {
+    let percentage = index * -100;
+    flexbox.style.transform =
+        "trasnlateX(" + porcentage + "%)";
+    index++;
+}, 1000);*/
